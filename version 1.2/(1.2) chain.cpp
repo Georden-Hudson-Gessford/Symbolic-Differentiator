91ee93068f943fddd7f8f0fc9aa1761162703a83
Georden-Hudson-Gessford <154949008+Georden-Hudson-Gessford@users.noreply.github.com>
2026-08-07 13:55:54 -0700

    Refactor variable names and include Parser.h

--- version 1.2/(1.2) chain.cpp
+++ version 1.2/(1.2) chain.cpp
@@ -1,17 +1,16 @@
-
 #include <iostream>
 #include "Differentiator.h"
-#include "parser.h"
+#include "Parser.h"
 #include "Route.h"
-
+// g++ chain.cpp Differentiator.cpp  Poly_differ.cpp Route.cpp  Parser.cpp -o we
 constexpr std::string::size_type not_found = std::string::npos;
 
 typedef std::string derivative;
 typedef std::string original;
-typedef std::string temp;
+typedef std::string temporary_variable;
+typedef int temporary_int;
 
 std::string factor_finder(std::string& e) {
-std::cout << "set " << e << "\n";
 size_t co_st = e.find_first_of("-1234567890");
 size_t co_end = e.find_last_of("1234567890");
 std::string co = "";
@@ -21,7 +20,6 @@ if ( (co_st != not_found && co_end != not_found) && (co_st == co_end))
 
 else if  ((co_st != not_found && co_end != not_found) && (co_st != co_end))
     co = e.substr(co_st, co_end + 1);
-std::cout << "sepcial fact " << co << "\n";
 return co;
 
 
@@ -82,47 +80,48 @@ int depth = 0;
 
 std::string chain_rule(std::string& e) {
 
-std::string special_factor = "";
-std::string outer_exp= "";
+std::string factor = "";
+std::string outer_exponent= "";
 
 original u;
 original v;
-derivative u_p;
-derivative v_p;
+derivative u_prime;
+derivative v_prime;
 
 std::string answer;
 u = find_u(e);
 v = find_v(e);
 
 if (u.find("^") != not_found) 
-    special_factor = factor_finder(u);
+    factor = factor_finder(u);
 
 
-if (special_factor != "") 
+if (factor != "") 
 {
-    int temp = 0;
-    temp = stoi(special_factor);
+    temporary_int temp = 0;
+    temp = stoi(factor);
     temp = temp - 1;
-    outer_exp = std::to_string(temp);
-    outer_exp = "^" + outer_exp;
+    outer_exponent = std::to_string(temp);
+    outer_exponent = "^" + outer_exponent;
 }
 
-temp duble_u = u;
-temp duble_v = v;
+temporary_variable d_du = u;
+temporary_variable v_dv = v;
 
-v_p = mother(duble_v);
+v_prime = recursive_route(v_dv);
 
-u_p = special_factor + differentiator(duble_u);
-
-
+u_prime = factor + main_differentiator(d_du);
 
 
 
 
-v_p.erase(0, v_p.find_first_not_of(" \t\n\r"));
-answer = v_p + "*" + u_p + "(" + v + ")" + outer_exp ;
+
+
+v_prime.erase(0, v_prime.find_first_not_of(" \t\n\r"));
+answer = v_prime + "*" + u_prime + "(" + v + ")" + outer_exponent ;
 answer = "(" + answer + ")";
 return answer;
 
 
 }
+
